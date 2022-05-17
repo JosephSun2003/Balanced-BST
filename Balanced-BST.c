eeac18b4303d6ab704595e340c7e1e3b66c3936e
Joseph Sun <jsphsun@gmail.com>
2022-05-17 13:02:44 -0400

    Added the Struct of the nodes used.

--- Balanced-BST.c
+++ Balanced-BST.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
+
+typedef struct BST_node_struct
+{
+    int key;
+    struct BST_node_struct* left;
+    struct BST_node_struct* right;
+}BST_node;
 
 int main()
 {
