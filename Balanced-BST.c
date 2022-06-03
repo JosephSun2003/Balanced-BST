df26249d085afe47d449ec91cf43526b30b04849
Joseph Sun <jsphsun@gmail.com>
2022-06-03 14:46:50 -0400

    Added an 'run' file and added node creation

--- Balanced-BST.c
+++ Balanced-BST.c
@@ -8,7 +8,22 @@ typedef struct BST_node_struct
     struct BST_node_struct* right;
 }BST_node;
 
-int main()
-{
-    printf("Hello world!");
+BST_node *new_BST_Node(int input_int){
+    /**
+     * @brief Creating a new node given an input value
+     * 
+     */
+
+    BST_node *new_node = (BST_node *)calloc(1,sizeof(BST_node));
+
+    //there's no more space chief
+    if(new_node == NULL) {
+        printf("No more space in Memory! \n");
+        return NULL;
+    }
+
+    //setting values
+    new_node->key = input_int;
+
+    return new_node;
 }
---
+++ run.c
@@ -0,0 +1,24 @@
+//gonna copy a lot of code from the BST Assignment from CSCA48
+
+#include<string.h>
+#include<Balanced-BST.c>
+
+int main()
+{
+    BST_node *root = NULL;
+    int choice = -1;
+
+    while (choice != 6){
+        printf("Please select from among the following options:\n");
+        printf("0 - Insert new note\n");
+        printf("1 - Search for note\n");
+        printf("2 - Delete note\n");
+        printf("3 - Print notes in order\n");
+        printf("4 - Print notes in pre-order\n");
+        printf("5 - Print notes in post-order\n");
+        printf("6 - Delete BST and exit\n");
+
+        scanf("%d",&choice); //input proccessing
+        getchar();// deal with  \n
+    }
+}
